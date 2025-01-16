0a7a22b0305f2b10927efafd99c74424330b9651
its-me-ojas <crestfallenwizard@gmail.com>
2025-01-16 16:12:38 +0530

    Count substrings that satify K-constraint I

---
+++ leetcode/3258.cpp
@@ -0,0 +1,23 @@
+#include <string>
+#include <unordered_map>
+class Solution {
+public:
+  int countKConstraintSubstrings(std::string s, int k) {
+    int count = 0;
+    for (int i = 0; i < s.size(); i++) {
+      int count0 = 0, count1 = 0;
+      for (int j = i; j < s.size(); j++) {
+        if (s[j] == '0') {
+          count0++;
+        } else {
+          count1++;
+        }
+        // after this you will get the freq count of the substring
+        if (count0 <= k || count1 <= k) {
+          count++;
+        }
+      }
+    }
+    return count;
+  }
+};
